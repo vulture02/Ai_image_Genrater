3c91685661915bf44ee9dadf0129272e9d5d0b39
vulture02 <amithp0210@gmail.com>
2025-09-26 06:22:46 +0530

    line add

--- frontend/src/Components/FromField.jsx
+++ frontend/src/Components/FromField.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 
-const FromField = () => {
+const FromField = ({labelName,type,name,placeholder,value,handleChange,isSurpriseMe,handelSurpriseMe}) => {
+  
   return (
-    <div>FromField</div>
+    <div>
+      <div className='flex items-center gap-2 mb-2'>
+        <label htmlFor={name} className='block text-sm font-medium text-gray-900'>{labelName}</label>
+        {isSurpriseMe &&(
+          <button type='button' onClick={handelSurpriseMe} className='font-semibold text-xs bg-[#ececf1] py-1 px-2 rounded-[5px] text-black'>Surprise Me</button>
+        )}
+
+      </div>
+    </div>
   )
 }
 
